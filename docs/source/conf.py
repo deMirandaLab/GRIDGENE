ae941b7b73b8a39a0345628d8834feaf4baa2176
Ana Marta Sequeira <anamartasequeira94@gmail.com>
2025-06-10 23:33:30 +0100

    still the docs

---
+++ docs/source/conf.py
@@ -0,0 +1,68 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+import os
+import sys
+project_root = os.path.abspath('../../')
+sys.path.insert(0, os.path.abspath('../..'))
+
+
+# ONLY add the 'src' directory to the Python path for autodoc to find your 'src' package
+
+project = 'GRIDGEN'
+copyright = '2025, AM Sequeira'
+author = 'AM Sequeira'
+release = '0.1'
+
+nb_execution_mode = "off"
+nbsphinx_allow_errors = True
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.mathjax',
+    'myst_nb',  # Jupyter Notebook support
+
+]
+
+autosummary_generate = True
+autodoc_default_options = {
+    'members': True,
+    'undoc-members': True,
+    'show-inheritance': True,
+}
+
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+autodoc_member_order = 'bysource'
+html_theme = 'pydata_sphinx_theme'
+html_static_path = ['_static']
+# html_theme = 'sphinx_rtd_theme'
+
+html_theme_options = {
+    "show_toc_level": 2, # Show current TOC up to 2 levels deep
+    "navbar_end": ["theme-switcher", "navbar-icon-links"], # PyData theme default, often includes search/index
+    # You might want to customize these:
+    # "secondary_sidebar_items": ["page-toc", "searchbox"], # Controls right sidebar items
+    # "header_links_before_dropdown": ["Home", "About"], # Example custom links
+}
+
+# Source suffix for Markdown files
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.md': 'markdown',
+}
---
+++ docs/source/index.rst
@@ -0,0 +1,18 @@
+.. GRIDGEN documentation master file, created by
+   sphinx-quickstart on Fri May 23 23:57:24 2025.
+
+Welcome to GRIDGEN's documentation!
+===================================
+
+GRIDGEN is a tool for extracting, organizing, and visualizing transcriptomic spatial data.
+
+It is designed to help researchers and data scientists work with spatial transcriptomics data efficiently.
+
+
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+   Tutorials <tutorials/index>
+   API Reference <api/index>
