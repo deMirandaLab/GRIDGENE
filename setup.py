5e4b86de41adc09b980ff38dc58e6ad608a0d5b3
Ana Marta Sequeira <anamartasequeira94@gmail.com>
2025-08-28 16:43:47 +0100

    Rename package to GRIDGENE - setup

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="gridgen",
+    name="gridgene",
     version="0.1.0",
     author="AM Sequeira",
-    description="GRIDGEN project",
+    description="GRIDGENE project",
     packages=find_packages(),
     install_requires=[
         "numpy",
